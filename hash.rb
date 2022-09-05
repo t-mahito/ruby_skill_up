c2833f11276a38f42df3f7f8bbbff5e35681e417
t-mahito <qqna42p9@gmail.com>
2022-09-05 23:51:02 +0900

    Update hash.rb

--- hash.rb
+++ hash.rb
@@ -5,3 +5,21 @@ array = [2,3]
 array << 1
 
 puts array
+
+# arrayからランダムで抜き出す
+array2 = []
+
+ array2 << array[rand(array.size)]
+
+ 
+
+puts array2
+
+puts array
+
+
+# arrayから削除する
+
+array.delete(1)
+
+puts array
