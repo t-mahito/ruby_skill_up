3030f8853568e69fa344cbe57f88205fc32b83e2
t-mahito <qqna42p9@gmail.com>
2022-09-06 15:32:08 +0900

    Create turukame3.rb

---
+++ turukame3.rb
@@ -0,0 +1,19 @@
+def turukame(inputa,inputb,sougaku,sousu)
+ inputa * 
+
+end
+
+
+puts "一つ目の値段を入力してください"
+inputa = gets.to_i
+
+puts "二つ目の値段を入力してください"
+inputb = gets.to_i
+
+puts "購入総額を教えてください"
+sougaku = gets.to_i
+
+puts "購入総数を教えてください"
+sousu = gets.to_i
+
+turukame(inputa,inputb,sougaku,sousu)
