c4ba840a5cfb03bfac096676c1de7926335df651
t-mahito <qqna42p9@gmail.com>
2022-09-15 15:46:53 +0900

    Create hash5.5.rb

---
+++ hash5.5.rb
@@ -0,0 +1,34 @@
+#2つの数字を入力
+#1つ目の数字を+1した数字を2つ目に指定した数字分保存する。
+
+def decide(a,b,array)
+  count = 0
+  
+ while b > count 
+  array << a + count
+    count += 1   
+  end
+  
+end
+array3 = []
+
+array2 = []
+
+array = []
+
+input = gets
+
+input = input.split
+
+a,b = input
+
+a = a.to_i
+b = b.to_i
+
+decide(a,b,array)
+
+array3 << array
+
+array2 << array
+
+puts array2
